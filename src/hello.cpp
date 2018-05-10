0b5f48ebcb9d351efede863686d137f1170536c0
Valtteri Ranta <valtterih.ranta@gmail.com>
2018-05-10 23:07:53 +0300

    Initialized the project

---
+++ src/hello.cpp
@@ -0,0 +1,20 @@
+/*
+ * Author: Valtteri Ranta, github: waldeog
+ *
+ * This project implements the CS106B assignment 5b: a general sorting algorithm other than selection,
+ * insertion, merge, or quicksort
+ */
+
+#include <iostream>
+#include "console.h"
+#include "simpio.h"
+
+using namespace std;
+
+
+int main() {
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+
