c4be40e4a1e5a9f92dfb925a9f60eccfb2487586
Valtteri Ranta <valtterih.ranta@gmail.com>
2018-05-29 22:05:49 +0300

    Added implementation of heapsort, does not sort correctly

--- src/hello.cpp
+++ src/hello.cpp
@@ -40,73 +40,55 @@ void Sort(Vector<Type> &v, int (cmpFn)(Type, Type) = OperatorCmp) {
  * 4) Go to step 2 unless the considered range of the list is one element
  */
 
-/*
- * Pseudocode for the algorithm, from https://en.wikipedia.org/wiki/Heapsort
- *
- * procedure heapsort(a, count) is
-    input: an unordered array a of length count
+//Does not work
 
-    (Build the heap in array a so that largest value is at the root)
-    heapify(a, count)
-
-    (The following loop maintains the invariants that a[0:end] is a heap and every element
-     beyond end is greater than everything before it (so a[end:count] is in sorted order))
-    end ← count - 1
-    while end > 0 do
-        (a[0] is the root and largest value. The swap moves it in front of the sorted elements.)
-        swap(a[end], a[0])
-        (the heap size is reduced by one)
-        end ← end - 1
-        (the swap ruined the heap property, so restore it)
-        siftDown(a, 0, end)
- *
- * USES TWO SUBROUTINES; heapify(a, count) and siftDown(a, start, end):
- *
- * Heapify:
- *
- * (Put elements of 'a' in heap order, in-place)
-procedure heapify(a, count) is
-    (start is assigned the index in 'a' of the last parent node)
-    (the last element in a 0-based array is at index count-1; find the parent of that element)
-    start ← iParent(count-1)
-
-    while start ≥ 0 do
-        (sift down the node at index 'start' to the proper place such that all nodes below
-         the start index are in heap order)
-        siftDown(a, start, count - 1)
-        (go to the next parent node)
-        start ← start - 1
-    (after sifting down the root all nodes/elements are in heap order)
- *
- *
- * siftDown:
- *
- * (Repair the heap whose root element is at index 'start', assuming the heaps rooted at its children are valid)
-procedure siftDown(a, start, end) is
-    root ← start
-
-    while iLeftChild(root) ≤ end do    (While the root has at least one child)
-        child ← iLeftChild(root)   (Left child of root)
-        swap ← root                (Keeps track of child to swap with)
-
-        if a[swap] < a[child]
-            swap ← child
-        (If there is a right child and that child is greater)
-        if child+1 ≤ end and a[swap] < a[child+1]
-            swap ← child + 1
-        if swap = root
-            (The root holds the largest element. Since we assume the heaps rooted at the
-             children are valid, this means that we are done.)
-            return
-        else
-            swap(a[root], a[swap])
-            root ← swap            (repeat to continue sifting down the child now)
- *
- */
-
-void HeapSort(vector<int> & v) {
-    //TODO: Implement
+void swap(Vector<int> & vec, int i, int j) {
+    int temp = vec[i];
+    vec[i] = vec[j];
+    vec[j] = temp;
 }
+
+void BuildMaxHeap(Vector<int> & vec, int n, int i) {
+    int max = i; //largest initialized as root
+    int left = 2*i + 1; //Left child position can be calculated from root with 2*i+1
+    int right = 2*i + 2; //Right child position can be calculated from root with 2*i+2
+
+    //If left child is larger than root
+    if (left < n && vec[left] > vec[max]) {
+        max = left;
+    }
+
+    //If right is larger than the largest so far
+    if (right < n && vec[right] > vec[max]) {
+        max = right;
+    }
+
+    //If max is not root
+    if (max != i) {
+        swap(vec, i, max);
+
+        //Recursively heapify the affected sub-tree down
+        BuildMaxHeap(vec, n, max);
+    }
+}
+
+void HeapSort(Vector<int> & v) {
+    //Build the heap (rearrange the vector so that at index i, left child = 2*i+1, right child = 2*i+2
+    int n = v.size();
+    for (int i = n/2-1; i >= 0; i--) {
+        BuildMaxHeap(v, n, i);
+    }
+
+    //One by one extract an element from heap
+    for (int i=n-1; i >= 0; i--) {
+        //Current root to the end
+        swap(v, 0, i);
+
+        //Heapify the reduced heap
+        BuildMaxHeap(v, n, 0);
+    }
+}
+
 
 // Generates a vector full of random integers between 1 and 10000
 Vector<int> intVecGenerator(int length) {
@@ -118,10 +100,19 @@ Vector<int> intVecGenerator(int length) {
     return vec;
 }
 
+void printVec(Vector<int> & v) {
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << endl;
+    }
+}
+
 int main() {
     Vector<int> vecToSort = intVecGenerator(sortSize);
 
     cout << "Vector size: " << vecToSort.size() << endl;
+
+    HeapSort(vecToSort);
+    printVec(vecToSort);
 
     /*
     // timing purposes: program start time in seconds
